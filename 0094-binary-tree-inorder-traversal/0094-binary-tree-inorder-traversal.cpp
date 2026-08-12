9fc72f27dcac326a8753ea673a237c49b1a16a5e
Shrishti Singh <singhshrishti76071@gmail.com>
2026-08-12 10:32:34 +0530

    Time: 0 ms (100%), Space: 11 MB (34.87%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -9,17 +9,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+//  MOORRIS TRAVERSAL
 class Solution {
 public:
-    void solve(TreeNode* root,vector<int>&ans){
-        if(root==NULL) return;
-        solve(root->left,ans);
-        ans.push_back(root->val);
-        solve(root->right,ans);
-    }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int>ans;
-        solve(root,ans);
+        TreeNode* curr=root;
+        if(root==NULL) return ans;
+        while(curr!=NULL){
+            
+            if(curr->left==NULL){
+                ans.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                TreeNode* pred=curr->left;
+                while(pred->right!=NULL&&pred->right!=curr){
+                    pred=pred->right;
+                }
+                if(pred->right==NULL){
+                    pred->right=curr;
+                    curr=curr->left;
+                }
+                else{
+                    pred->right=NULL;
+                    ans.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
+        }
         return ans;
     }
 };
