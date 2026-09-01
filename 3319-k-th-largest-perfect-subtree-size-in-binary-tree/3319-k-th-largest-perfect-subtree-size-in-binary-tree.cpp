28c9f9d67ab82fd86bee1e80a38312bf7eb783c3
Shrishti Singh <singhshrishti76071@gmail.com>
2026-09-01 14:22:29 +0530

    Time: 5 ms (73.01%), Space: 66.4 MB (49.64%) - LeetHub

---
+++ 3319-k-th-largest-perfect-subtree-size-in-binary-tree/3319-k-th-largest-perfect-subtree-size-in-binary-tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>PBTsize;
+    pair<bool,int>dfs(TreeNode * root){
+        if(root==NULL) return{true,0};
+        auto left=dfs(root->left);
+        auto right=dfs(root->right);
+
+        if(left.first && right.first && left.second==right.second){
+            int size=left.second+right.second+1;
+            PBTsize.push_back(size);
+            return {true,size};
+        }
+        return {false,0};
+    }
+    int kthLargestPerfectSubtree(TreeNode* root, int k) {
+       dfs(root); 
+       sort(PBTsize.begin(),PBTsize.end(),greater<int>());
+       if(k>PBTsize.size()){
+       return -1;
+       }
+       
+       return PBTsize[k-1];
+    }
+};
