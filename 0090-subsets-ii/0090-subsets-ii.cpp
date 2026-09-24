1fb9f9afb7a95410e9adc99d8bd50ce1ab18fa72
Shrishti Singh <singhshrishti76071@gmail.com>
2026-09-24 14:25:19 +0530

    Time: 1 ms (44.89%), Space: 10.9 MB (23.64%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
+    vector<vector<int>> ans;
     vector<int>temp;
-    vector<vector<int>>ans;
     void solve(int index,vector<int>& nums){
         ans.push_back(temp);
         for(int i=index;i<nums.size();i++){
-            if(i>index&&nums[i]==nums[i-1]) continue;
+            if(i>index&&nums[i]==nums[i-1])  continue;
             temp.push_back(nums[i]);
             solve(i+1,nums);
             temp.pop_back();
